43ea25e88a86073af8c22717106d67c7d8d55c7f
Lia Papailiou <evangelia.papailiou@gmail.com>
2020-09-24 13:02:46 +0200

    set up models, postgres and change admin display

--- ecorusOfficeDemo/office/admin.py
+++ ecorusOfficeDemo/office/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Person, Office
 
-# Register your models here.
+
+class PersonAdmin(admin.ModelAdmin):
+  list_display = ('person_name', 'person_age')
+
+
+class OfficeAdmin(admin.ModelAdmin):
+  list_display = ('office_name', 'peopleWorking')
+
+
+admin.site.register(Person, PersonAdmin)
+admin.site.register(Office, OfficeAdmin)
--- ecorusOfficeDemo/office/models.py
+++ ecorusOfficeDemo/office/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
-# Create your models here.
+
+class Person(models.Model):
+  person_name = models.CharField(max_length=20)
+  person_age = models.IntegerField()
+
+  def happyBirthday(self):
+    self.person_age += 1
+    return self.person_age
+  
+  def changeName(self, new_name):
+    self.person_name = new_name
+    return self.person_name
+
+
+class Office(models.Model):
+  office_name = models.CharField(max_length=50)
+  peopleWorking = ArrayField(models.CharField(max_length=20),default=[], blank=True)
+
+  def startWorkingFor(self, person=Person):
+    self.peopleWorking.append(person.person_name)
+  
+  def finishedWorkingFor(self, person=Person):
+    self.peopleWorking.remove(person.person_name)
+
