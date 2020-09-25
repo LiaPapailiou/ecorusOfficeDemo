9f276c8b67db482a226637ace608bf126c813210
Lia Papailiou <evangelia.papailiou@gmail.com>
2020-09-25 22:33:00 +0200

    add logout

--- ecorusOfficeDemo/accounts/views.py
+++ ecorusOfficeDemo/accounts/views.py
@@ -37,3 +37,14 @@ class LoginAPI(generics.GenericAPIView):
                 "token": AuthToken.objects.create(user)[1],
             }
         )
+
+
+class UserAPI(generics.RetrieveAPIView):
+    permission_classes = [
+        permissions.IsAuthenticated,
+    ]
+    serializer_class = UserSerializer
+
+    def get_object(self):
+        return self.request.user
+
